c41e8daf275b686eb40db0ad24d59bc8bf8292be
Matilda Zhang <mzhang.142857@gmail.com>
2022-05-26 13:50:26 -0400

    authentication phase 1 done

---
+++ authenticate-me/backend/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+router.get('/hello/world', csrfProtection, function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+module.exports = router;
