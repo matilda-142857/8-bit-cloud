6f70ce93dab356e8cd37ce5504964e0cfc44f31b
Matilda Zhang <mzhang.142857@gmail.com>
2022-05-26 14:40:12 -0400

    authentication pt1 done

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 // // GET /api/set-token-cookie
 // const asyncHandler = require('express-async-handler');
